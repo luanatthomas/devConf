ac20ad92589bba603ed0674d16096c958188e318
nandoFontanive <nando.fontanive@gmail.com>
2023-04-13 15:28:44 -0300

    TestePageView white stripe fix

--- devConf/devConf/TestePageView.swift
+++ devConf/devConf/TestePageView.swift
@@ -10,35 +10,40 @@ import SwiftUI
 struct TestePageView: View {
     var body: some View {
         NavigationStack{
-            TabView {
-                Onboarding1()
-                    .tabItem {
-                        Image(systemName: "circle")
-                        Text("Page 1")
-                    }
-                    .ignoresSafeArea(.all)
-
-                Onboarding2()
-                    .tabItem {
-                        Image(systemName: "circle")
-                        Text("Page 2")
-                    }
-                    .ignoresSafeArea(.all)
-                
-                Onboarding3()
-                    .tabItem {
-                        Image(systemName: "circle")
-                        Text("Page 3")
-                    }
-                    .ignoresSafeArea(.all)
-
+            ScrollView {
+                TabView {
+                    Onboarding1()
+                        .tabItem {
+                            Image(systemName: "circle")
+                            Text("Page 1")
+                        }
+                        .ignoresSafeArea(.all)
+                    
+                    Onboarding2()
+                        .tabItem {
+                            Image(systemName: "circle")
+                            Text("Page 2")
+                        }
+                        .ignoresSafeArea(.all)
+                    
+                    Onboarding3()
+                        .tabItem {
+                            Image(systemName: "circle")
+                            Text("Page 3")
+                        }
+                        .ignoresSafeArea(.all)
+                }
+                        .frame(
+                            width: UIScreen.main.bounds.width ,
+                            height: UIScreen.main.bounds.height
+                        )
+                                        .tabViewStyle(PageTabViewStyle())
+                }
+//                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                            .edgesIgnoringSafeArea(.all)
             }
-            //.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                .ignoresSafeArea(.all)
         }
     }
-}
-
 struct TestePageView_Previews: PreviewProvider {
     static var previews: some View {
         TestePageView()
